40583086a8734bb624c3ccf234b2d10ea5c77b1f
Niklas Vd <niklas.vd123@gmail.com>
2015-12-05 22:18:02 +0100

    Added a DB project, databases and AWS storage API comes in there

---
+++ Caseomatic Networking/Caseomatic.Db/AWS/AWSServiceCredentials.cs
@@ -0,0 +1,24 @@
+﻿using Amazon.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Caseomatic.Db.AWS
+{
+    public static class AWSServiceCredentials
+    {
+        private static AWSCredentials credentials;
+        public static AWSCredentials Credentials
+        {
+            get { return credentials; }
+            private set { credentials = value; }
+        }
+
+        public static void SetCredentials(AWSCredentials newCredentials)
+        {
+            credentials = newCredentials;
+            // Check for validity?
+        }
+    }
+}
